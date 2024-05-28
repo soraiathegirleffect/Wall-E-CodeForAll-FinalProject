db708e32914bd294536e6c57aa9f2e856ca14815
soraiathegirleffect <soraialobos@hotmail.com>
2024-05-28 20:40:30 +0100

    testing planets

--- app/services/planetService.js
+++ app/services/planetService.js
@@ -6,15 +6,30 @@ const API_PLANETS3 = "https://swapi.dev/api/planets/?page=3"
 const API_PLANETS4 = "https://swapi.dev/api/planets/?page=4"
 const API_PLANETS5 = "https://swapi.dev/api/planets/?page=5"
 const API_PLANETS6 = "https://swapi.dev/api/planets/?page=6"
+//const result = JSON.parse(json);
+//result.results.push(...json.results);
 
+async function planetService(){
+    const response = await fetch(`${API_PLANETS1}`);
 
-function planetService(){
-    const response = fetch(`${API_PLANETS1}`)
-    .then(function(response){
-        return console.log(response.json());
-    })
+    const planetData = await response.json();
+
+    return planetData;
     
 }
+
+function nameService(){
+planetService()
+.then(planetData => {
+    var results = planetData.results;
+    results.forEach(result =>  console.log(result.name)); })
+};
+
+
+nameService();
+
+
+
 
 
 
