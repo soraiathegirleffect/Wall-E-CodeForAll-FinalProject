00e8cc539d7f0b0a9347bb232db30e1fd66bece1
soraiathegirleffect <soraialobos@hotmail.com>
2024-05-29 19:31:09 +0100

    goto fixed html

--- app/main.js
+++ app/main.js
@@ -6,7 +6,7 @@ import { learnPlanets } from "./views/learnPlanets.js"
 import { teachPlanets } from "./views/teachPlanets.js"
 import { cleanPlanets } from "./views/cleanPlanets.js"
 
-import { planetService } from "./views/planetService.js"
+import { planetService } from "./services/planetService.js"
 
 
 const sections = [
@@ -50,9 +50,9 @@ const routes = [
     //{ url:"/error", page: error }
 ]; // if any of the above are not correctly defined the whole page wont load
 
-render();
+
 planetService()
-//goto(document.location.pathname); // here we have defensive programming so we changed here instead of render
+goto(document.location.pathname); // here we have defensive programming so we changed here instead of render
 window.addEventListener('popstate',render);
 
 function goto(url){
