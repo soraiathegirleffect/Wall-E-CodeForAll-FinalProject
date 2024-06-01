7aac44cb95bcfde26a9e16dcaa408b25600f1e43
soraiathegirleffect <soraialobos@hotmail.com>
2024-06-01 14:52:53 +0100

    fixed property types

--- Wall-E-RestAPI/src/main/java/org/example/services/PlanetServiceImpl.java
+++ Wall-E-RestAPI/src/main/java/org/example/services/PlanetServiceImpl.java
@@ -13,12 +13,13 @@ public class PlanetServiceImpl implements PlanetService {
 
     private static List<Planet> planets = new ArrayList<>();
 
+
     static {
-        planets.add(new Planet("Earth", "Temperate", 12742.0, 7800000000.0, "Various"));
-        planets.add(new Planet("Mars", "Arid", 6779.0, 0.0, "Rocky"));
-        planets.add(new Planet("Venus", "Hot", 12104.0, 0.0, "Volcanic"));
-        planets.add(new Planet("Jupiter", "Gas Giant", 139820.0, 0.0, "Gaseous"));
-        planets.add(new Planet("Saturn", "Gas Giant", 116460.0, 0.0, "Gaseous"));
+        planets.add(new Planet("Earth", "Temperate", 12742, "7800000000", "Various"));
+        planets.add(new Planet("Mars", "Arid", 6779, "0", "Rocky"));
+        planets.add(new Planet("Venus", "Hot", 12104, "0", "Volcanic"));
+        planets.add(new Planet("Jupiter", "Gas Giant", 139820, "0", "Gaseous"));
+        planets.add(new Planet("Saturn", "Gas Giant", 116460, "0", "Gaseous"));
     }
 
 
